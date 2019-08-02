2b9e9b04e386f49e84d55f7df270273004ecb7a7
jesus <jesus_e1992@hotmail.com>
2019-08-02 17:04:44 -0400

    subiendo webapi

---
+++ APIWeb/Controllers/usersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIWeb.Models;
+
+namespace APIWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public usersController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.users.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.users.Add(new user { UserName="jesus92", Name = "jesus", LastName = "Enrique", Age = 27 });
+                _context.SaveChanges();
+            }
+        }
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<user>>> Getusers()
+        {
+            return await _context.users.ToListAsync();
+        }
+
+        // GET: api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<user>> Getuser(long id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<user>> Postuser(user u)
+        {
+            _context.users.Add(u);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Getuser), new { id = u.userId }, u);
+        }
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putuser(long id, user item)
+        {
+            if (id != item.userId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deleteuser(long id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
+
---
+++ APIWeb/Models/users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIWeb.Models
+{
+    public class user
+    {
+        public long userId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime LastSessionDateTime { get; set; }
+
+    }
+}
